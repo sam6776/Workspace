c004775d0f2b1a7b6577736d813c3bb529dd91b4
sam <sforde6776@gmail.com>
2020-09-21 09:44:32 +0000

    fd

--- python/machineLearning/neuralNet.py
+++ python/machineLearning/neuralNet.py
@@ -2,3 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 input = np.linspace(-10,10,100)
+
+def sigmoid(X):
+    val = 1/(1+np.exp(-X))
+    return val
+
+output = sigmoid(input)
+plt.plot(input, output)
+plt.xlabel("Input")
+plt.ylabel("Output")
+plt.title("Sigmoid Function")
