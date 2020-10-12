04790c248ddabf225a87d1d9880d18bb0e3e4741
sam <sforde6776@gmail.com>
2020-10-12 09:30:12 +0000

    sdf

--- APCS/lockClass/Coin.java
+++ APCS/lockClass/Coin.java
@@ -11,6 +11,8 @@ public class Coin implements lockable
 {
    private final int HEADS = 0;
    private final int TAILS = 1;
+   private int key;
+   private boolean isLocked;
 
    private int face;
 
@@ -19,15 +21,37 @@ public class Coin implements lockable
    //-----------------------------------------------------------------
    public Coin ()
    {
-      flip();
+      key = 0;
+      isLocked = false;
+      if(!(isLocked))
+      {
+         flip();
+      }
    }
 
+   /** Coin
+    * Overloaded constructor that takes the key and the state of being locked as an input
+    * @param key the key for the lock
+    * @param locked the status of the method
+    */
+   public Coin(int key, boolean locked)
+   {
+      this.key = key;
+      isLocked = locked;
+      if(!(isLocked))
+      {
+         flip();
+      }
+   }
    //-----------------------------------------------------------------
    //  Flips the coin by randomly choosing a face value.
    //-----------------------------------------------------------------
    public void flip ()
    {
-      face = (int) (Math.random() * 2);
+      if(!(isLocked))
+      {
+         face = (int) (Math.random() * 2);
+      }
    }
 
    //-----------------------------------------------------------------
@@ -44,35 +68,57 @@ public class Coin implements lockable
    public String toString()
    {
       String faceName;
-      if (face == HEADS)
-         faceName = "Heads";
+      if(!(isLocked))
+      {
+         if (face == HEADS)
+         {
+            faceName = "Heads";
+         }  
       else
+      {
          faceName = "Tails";
-
+      }
+      return faceName;
+      }
+      faceName = "The class is currently locked, pleace unlock it.";
       return faceName;
    }
 
     @Override
     public void lock(int key) 
     {
-        // TODO
+        if(this.key == key)
+        {
+           isLocked = true;
+        }
+        else
+        {
+           System.out.println("The key given is incorrect.");
+        }
     }
 
     @Override
     public void unlock(int key)
      {
-        // TODO
+        if(this.key == key)
+        {
+           isLocked = false;
+        }
+        else
+        {
+           System.out.println("The key given is incorrect.");
+        }
     }
 
     @Override
     public void setKey(int key)
      {
-        // TODO
+        this.key = key;
     }
 
     @Override
     public boolean locked() 
     {
-        // TODO
+        return isLocked;
     }
 }
